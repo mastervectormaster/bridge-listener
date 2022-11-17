734cd917f8bbeffb45e8f02bd5d02f6e23766111
Vectormaster <vectormaster123456789@gmail.com>
2022-11-17 13:21:28 -0600

    finish

--- src/ws.js
+++ src/ws.js
@@ -1,12 +1,13 @@
 const ethers = require("ethers");
 const { Network, Alchemy } = require("alchemy-sdk");
+const { exec } = require("child_process");
 require("dotenv").config();
 
 const BRIDGE_CONTRACT_ADDR = process.env.BRIDGE_CONTRACT_ADDR;
 const BRIDGE_EVENT_TOPIC = ethers.utils.id(
   "SendToCosmosEvent(address,address,string,uint256)"
 );
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
 const iface = new ethers.utils.Interface([
   "event SendToCosmosEvent(address indexed token, address indexed sender, string recipient, uint256 amount)",
@@ -22,13 +23,28 @@ var filter = {
   topics: [BRIDGE_EVENT_TOPIC],
 };
 
-console.log("-----   Listener Start  -------")
+console.log("-----   Listener Start  -------");
 
 alchemy.ws.on(filter, (data) => {
-  console.log("Event from Bridge Received")
-  const {token, sender, recipient, amount} = iface.decodeEventLog("SendToCosmosEvent", data.data, data.topics);
+  console.log("Event from Bridge Received");
+  const { token, sender, recipient, amount } = iface.decodeEventLog(
+    "SendToCosmosEvent",
+    data.data,
+    data.topics
+  );
 
-  console.log(sender)
-  console.log(recipient)
-  console.log(amount.toNumber())
+  exec(
+    `blockxd tx bridge sent-to-blockx ${recipient} ${amount.toNumber()}abcx --from alice --chain-id blockx_12345-1`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
 });
